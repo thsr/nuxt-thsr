aba21efd080e862ac0efdffa27bb037b0b4c5b89
thsr <437808@gmail.com>
2018-01-23 18:08:47 -0500

    clean up hb route headers

--- server/hb/search/index.js
+++ server/hb/search/index.js
@@ -10,11 +10,9 @@ const router = Router()
 ==============================*/
 
 router.use(function (req, res, next) { 
-  res.setHeader('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL_ALLOW_ORIGIN_HB) // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Methods', 'GET') // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Credentials', true); // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
-  next() // Pass to next layer of middleware
+  res.setHeader('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL_ALLOW_ORIGIN_HB)
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  next()
 })
 
 
